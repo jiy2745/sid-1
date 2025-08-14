6bdc7e2ec18f4b3613fef4ba502135bf7bc84c2d
Jason Kim <jasonjiseop.kim@gmail.com>
2025-08-14 17:06:16 +0900

    feat : new slash minigame prototype

---
+++ Assets/Scripts/Minigame/NewSlash/SlashBar.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+public class SlashBar : MonoBehaviour
+{
+    public RectTransform pointer;   // Reference to the pointer
+    public float pointerSpeed = 5f; // Speed at which the pointer moves
+
+    public RectTransform safeZone; // Reference to the safe zone area
+    public float[] safeZoneWidths = { 150f, 100f, 60f }; // Widths for different difficulty levels
+
+    private bool movingRight = true; // Direction of pointer movement
+    private float barWidth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        barWidth = GetComponent<RectTransform>().rect.width; // Get the width of the bar
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePointer();
+    }
+
+    private void MovePointer()
+    {
+        float moveAmount = pointerSpeed * Time.deltaTime * (movingRight ? 1 : -1);
+        pointer.localPosition += new Vector3(moveAmount, 0, 0);
+
+        float halfBar = barWidth / 2;
+        if (pointer.localPosition.x >= halfBar)
+        {
+            pointer.localPosition = new Vector3(halfBar, pointer.localPosition.y, 0);
+            movingRight = false;
+        }
+        else if (pointer.localPosition.x <= -halfBar)
+        {
+            pointer.localPosition = new Vector3(-halfBar, pointer.localPosition.y, 0);
+            movingRight = true;
+        }
+    }
+
+    public void GenerateSafeZone(int difficulty)
+    {
+        float width = safeZoneWidths[difficulty];
+        safeZone.sizeDelta = new Vector2(width, safeZone.sizeDelta.y); // Set the width of the safe zone
+
+        // Random position within the bar width
+        float positionX = UnityEngine.Random.Range(-barWidth / 2 + width / 2, barWidth / 2 - width / 2);
+
+        // Reset pointer position
+        pointer.localPosition = new Vector3(-barWidth / 2, pointer.localPosition.y, 0);
+        movingRight = true; // Reset direction to right
+    }
+    
+    public bool CheckIfInSafeZone()
+    {
+        float pointerX = pointer.localPosition.x;
+        float safeLeft = safeZone.localPosition.x - (safeZone.sizeDelta.x / 2);
+        float safeRight = safeZone.localPosition.x + (safeZone.sizeDelta.x / 2);
+
+        return pointerX >= safeLeft && pointerX <= safeRight;
+    }
+}
---
+++ Assets/Scripts/Minigame/NewSlash/SlashMinigame.cs
@@ -0,0 +1,120 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SlashMinigame : MonoBehaviour
+{
+    [Header("References")]
+    public SlashBar slashBar; // Reference to the SlashBar component
+    public Animator playerAnimator; // Reference to the player's animator
+    public UnityEngine.UI.Image radialTimerImage; // Reference to the radial timer image
+    public UnityEvent onMinigameStop;
+
+    [Header("Settings")]
+    public int totalSegments = 5;
+    public int lives = 3; // Number of lives the player has
+    public float segmentTimelimit = 3f; // Time allowed for each segment
+
+    private int currentSegment = 0; // Current segment the player is on
+    private float minigameTimer;    // Timer for whole minigame
+    private bool gameActive = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        //TODO: Map to player object later
+        StartMinigame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameActive) return;
+
+        // --- Minigame timer logic --- 
+        minigameTimer -= Time.deltaTime;    // countdown timer
+        radialTimerImage.fillAmount = minigameTimer / segmentTimelimit; // Update the radial timer image
+        if (minigameTimer <= 0)
+        {
+            SegmentFailed();
+        }
+
+        // Input handling
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (playerAnimator != null)
+            {
+                playerAnimator.SetTrigger("Slash"); // Trigger the slash animation
+            }
+            bool success = slashBar.CheckIfInSafeZone(); // Check if the player slashed in the safe zone
+            if (success)
+            {
+                currentSegment++;
+                if (currentSegment >= totalSegments)
+                {
+                    StopMinigame(true); // Player won the minigame
+                }
+                else
+                {
+                    StartNewSegment(); // Start the next segment
+                }
+            }
+            else
+            {
+                SegmentFailed(); // Player failed the segment
+            }
+        }
+    }
+
+    private void SegmentFailed()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            StopMinigame(false); // Player lost the minigame
+        }
+        else
+        {
+            Debug.Log("Segment failed, lives left: " + lives);
+            StartNewSegment(); // Start a new segment
+        }
+    }
+
+    public void StartMinigame()
+    {
+        if (gameActive) return;
+        else
+        {
+            gameActive = true;
+            StartNewSegment();
+            Debug.Log("Minigame start");
+        }
+    }
+
+    public void StopMinigame(bool playerWon)
+    {
+        gameActive = false;
+        if (playerWon)
+        {
+            Debug.Log("Player Won");
+            // Do additional stuff when won
+        }
+        else
+        {
+            Debug.Log("Player Lost");
+            // Do additional stuff when lost
+        }
+        onMinigameStop?.Invoke();
+    }
+    
+       private void StartNewSegment()
+    {
+        int difficulty = UnityEngine.Random.Range(0, 3); // Randomly select a difficulty level
+        slashBar.GenerateSafeZone(difficulty);
+
+        // Reset the radial timer
+        minigameTimer = segmentTimelimit;
+        radialTimerImage.fillAmount = 1f; // Reset the radial timer image
+    }
+}
