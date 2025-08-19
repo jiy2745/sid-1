ef106348986e504f3011dac41327c9618a1b2e22
jason <jasonjiseop.kim@gmail.com>
2025-08-19 14:48:51 +0900

    feat : added space bar for slash minigame

---
+++ Assets/Scripts/Minigame/NewSlash/SlashSpaceBar.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlashSpaceBar : MonoBehaviour
+{
+    private Image spaceBarImage;
+    public Sprite spaceBarPressed;
+    public Sprite spaceBarLifted;
+    public float pressInterval = 1f;
+
+    private bool isPressed = false;
+    private Coroutine pulseCoroutine;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spaceBarImage = GetComponent<Image>();
+    }
+
+    void OnEnable()
+    {
+        if (pulseCoroutine == null)
+        {
+            pulseCoroutine = StartCoroutine(AlternateSprites());
+        }
+    }
+
+    void OnDisable()
+    {
+        if (pulseCoroutine != null)
+        {
+            StopCoroutine(AlternateSprites());
+            pulseCoroutine = null;
+        }
+    }
+
+    private IEnumerator AlternateSprites()
+    {
+        while (true)
+        {
+            spaceBarImage.sprite = isPressed ? spaceBarPressed : spaceBarLifted;
+            isPressed = !isPressed;
+            yield return new WaitForSeconds(pressInterval);
+        }
+    }
+}
